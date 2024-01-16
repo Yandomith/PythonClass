62343e41ba91bfc619a5d872f1feb7719aee2774
Yandomith <yandomith@gmail.com>
2024-01-16 20:39:28 +0545

    new app commeted

--- PythonSmithVenv/smith/smith/urls.py
+++ PythonSmithVenv/smith/smith/urls.py
@@ -20,5 +20,8 @@ from django.urls import include, path
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path('', include("pages.urls"))
+    
+    
     
 ]
