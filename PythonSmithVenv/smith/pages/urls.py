2d9723be12f9520f76334000af059e86b32245f0
Yandomith <yandomith@gmail.com>
2024-01-24 15:39:21 +0545

    frontend works

--- PythonSmithVenv/smith/pages/urls.py
+++ PythonSmithVenv/smith/pages/urls.py
@@ -2,5 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home, name= "home")
+    path('',views.home, name= "Home"),
+    path('policy',views.policy, name= "Policy"),
+    path('terms',views.terms, name = "Terms")
 ]
--- PythonSmithVenv/smith/pages/views.py
+++ PythonSmithVenv/smith/pages/views.py
@@ -5,4 +5,10 @@ from django.shortcuts import render
 def home(request):
     return render(request,"index.html")
 
+def policy (request):
+    return render (request, "policy.html")
+
+def terms (request):
+    return render (request, "terms.html")
+
 # PythonSmithVenv/smith/pages/templates/index.html
